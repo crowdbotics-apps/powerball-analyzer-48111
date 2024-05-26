3905c92ab528decc4827a0f5d8809e781507f724
Crowdbotics <team@crowdbotics.com>
2024-05-26 22:23:00 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "powerball-analyzer-48111",
+  url: "https://powerball-analyzer-48111.botics.co",
+  api: "https://powerball-analyzer-48111.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
